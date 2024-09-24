5f62b95a2738a957a17eac9ed3857c5d7c0ddad8
shrut2702 <shroot2702hd@gmail.com>
2024-09-25 02:32:30 +0530

    Updated

--- AI Pipeline/image_processing_serv.py
+++ AI Pipeline/image_processing_serv.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, make_response
+import gzip
+import io
 import requests
 
 app = Flask(__name__)
@@ -24,12 +26,25 @@ def upload_image():
             grouping_response = req_product_grouping(detected_products)
             
             if grouping_response['status_code'] == 200:
-                return jsonify(grouping_response['json']), 200
+                response_data = jsonify(grouping_response['json'])
+                
+                # Compress the response data
+                compressed_data = compress_response(response_data.get_data())
+                response = make_response(compressed_data)
+                response.headers['Content-Encoding'] = 'gzip'
+                response.headers['Content-Type'] = 'application/json'
+                return response, 200
             else:
                 return jsonify({'error': 'Product grouping failed'}), 500
         
         else:
             return jsonify({'error': 'Product detection failed'}), 500
+
+def compress_response(data):
+    buf = io.BytesIO()
+    with gzip.GzipFile(fileobj=buf, mode='wb') as f:
+        f.write(data)
+    return buf.getvalue()
 
 def req_product_detection(image_file):
     url = 'http://localhost:5001/detect'
@@ -44,4 +59,3 @@ def req_product_grouping(detected_products):
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
-
